a2d70313595bc8fedbfa2aadfdaabb58d10c2760
Angular2guy <angular2guy@gmail.com>
2023-05-06 11:05:06 +0200

    chore: update

--- frontend/src/angular/src/app/services/auth-guard.service.ts
+++ frontend/src/angular/src/app/services/auth-guard.service.ts
@@ -13,15 +13,11 @@
    limitations under the License.
  */
 import { Injectable } from "@angular/core";
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-} from "@angular/router";
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 
 @Injectable({ providedIn: "root" })
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
   constructor() {}
 
   canActivate(
