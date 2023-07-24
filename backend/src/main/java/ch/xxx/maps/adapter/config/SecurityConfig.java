5fa0b82b22eba518de10dd0d37d479572aa16937
Angular2guy <angular2guy@gmail.com>
2023-07-24 20:55:18 +0200

    fix: update security

--- backend/src/main/java/ch/xxx/maps/adapter/config/SecurityConfig.java
+++ backend/src/main/java/ch/xxx/maps/adapter/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter.HeaderValue;
 
 @Configuration
 @EnableWebSecurity
@@ -25,10 +26,15 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        HttpSecurity result = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.anonymous().and().headers()
-				.contentSecurityPolicy("default-src 'self' data: https://*.virtualearth.net https://*.bing.com; script-src 'self' https://*.virtualearth.net https://*.bing.com 'unsafe-inline'; style-src 'self' https://*.bing.com 'unsafe-inline'; font-src 'self' data: https://fonts.gstatic.com;")
-				.and().xssProtection().and().frameOptions().sameOrigin().and().csrf().disable();
+		HttpSecurity result = http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll())
+				.csrf(myCsrf -> myCsrf.disable())
+				.sessionManagement(mySm -> mySm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.headers(myHeaders -> myHeaders.contentSecurityPolicy(myCsp -> myCsp.policyDirectives(
+						"default-src 'self' data: https://*.virtualearth.net https://*.bing.com; script-src 'self' https://*.virtualearth.net "
+				        +" https://*.bing.com 'unsafe-inline'; style-src 'self' https://*.bing.com 'unsafe-inline'; font-src 'self' "
+						+" data: https://fonts.gstatic.com;")))
+				.headers(myHeaders -> myHeaders.xssProtection(myXss -> myXss.headerValue(HeaderValue.ENABLED)))
+				.headers(myHeaders -> myHeaders.frameOptions(myFo -> myFo.sameOrigin()));
 		return result.build();
 	}
 }
