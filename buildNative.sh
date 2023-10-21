697c842d79ff66689016f64b2487fe1c661737a2
Angular2guy <angular2guy@gmail.com>
2023-10-21 13:07:00 +0200

    fix: update native build

--- buildNative.sh
+++ buildNative.sh
@@ -22,9 +22,9 @@ java -Dspring.aot.enabled=true -agentlib:native-image-agent=config-merge-dir=bac
 #    },
 # native build
 ./gradlew clean nativeCompile -PwithAngular=true -PuseChromium=true
-# run native binary(more than 160MB)
+# run native binary(more than 200MB)
 ./backend/build/native/nativeCompile/backend
 # build Docker image
-docker build -t angular2guy/angularandspringwithmaps-native:latest --build-arg APP_FILE=native/nativeCompile/backend --no-cache .
+docker build -t angular2guy/angularandspringwithmaps-native:latest --build-arg APP_FILE=build/native/nativeCompile/backend --no-cache .
 # run Docker image
-docker run -p 8080:8080 --memory="210m" --network="host" angular2guy/angularandspringwithmaps-native:latest
+docker run -p 8080:8080 --memory="300m" --network="host" angular2guy/angularandspringwithmaps-native:latest
