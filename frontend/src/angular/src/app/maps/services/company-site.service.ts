0d73b6a7cf3cdd342afac0538eb39810afd80aa4
Angular2guy <angular2guy@gmail.com>
2024-08-03 12:34:55 +0200

    fix: cleanup

--- frontend/src/angular/src/app/maps/services/company-site.service.ts
+++ frontend/src/angular/src/app/maps/services/company-site.service.ts
@@ -13,7 +13,6 @@
    limitations under the License.
  */
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { CompanySite } from "../model/company-site";
