1715ced941ee2453e99cbd3f400e977a529773cc
Angular2guy <angular2guy@gmail.com>
2025-06-15 10:00:42 +0200

    feat: update

---
+++ helm/knativeSetup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# execute helmCommand.sh
+
+# Install Knative Cli kn: https://knative.dev/docs/getting-started/quickstart-install/#before-you-begin
+
+# Install Quickstart Plugin: https://knative.dev/docs/getting-started/quickstart-install/#before-you-begin
+
+# Install Func Cli: https://knative.dev/docs/getting-started/install-func/#__tabbed_1_2
+
+# minikube start -p knative 
+
